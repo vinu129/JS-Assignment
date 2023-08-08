030d3ab64eec66ab169ce9db0546a47537cb4760
VinayChudasama01 <vinay.chudasama@1rivet.com>
2023-08-08 10:19:49 +0530

    Find the LCS of given two strings

--- script.js
+++ script.js
@@ -72,3 +72,24 @@ function reverseString(str) {
     console.log("Reversed String :" + newString);
     return newString;
 }
+
+// Longest Common Subsequence //
+
+// function longestCommonSubsequence(a, b) {
+//     const matrix = Array(a.length + 1).fill().map(() => Array(b.length + 1).fill(0));
+//     for(let i = 1; i < a.length + 1; i++) {
+//         for(let j = 1; j < b.length + 1; j++) {
+//             if(a[i-1] === b[j-1]) {
+//                 matrix[i][j] = 1 + matrix[i-1][j-1];
+//             } else {
+//                 matrix[i][j] = Math.max(matrix[i-1][j], matrix[i][j-1]);
+//             }
+//         }
+//     }
+//     return matrix[a.length][b.length];
+// }
+
+// let a = [2,3,4];
+// let b = [2,3,7,8,4];
+
+// console.log(longestCommonSubsequence(a,b));
