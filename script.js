dac2c4b02e3e2198ccc66963d848aba97eac4d7c
VinayChudasama01 <vinay.chudasama@1rivet.com>
2023-08-09 16:57:58 +0530

    Maximum product of three numbers from array of integers

--- script.js
+++ script.js
@@ -179,3 +179,28 @@ function trailingZeros(n) {
     console.log("Number of zeros : " + count);
 
 }
+
+// Maximum product of three numbers 
+function maxProduct() {
+    let arr = [10, 3, 5, 6, 20, 30];
+    let n = arr.length;
+
+    if (n < 3)
+        return -1;
+
+    // will contain max product
+    let max_product = Number.MIN_VALUE;
+
+    for (let i = 0; i < n - 2; i++) {
+        for (let j = i + 1; j < n - 1; j++) {
+            for (let k = j + 1; k < n; k++) {
+                max_product = Math.max(max_product,
+                    arr[i] * arr[j] * arr[k]);
+            }
+        }
+    }
+    console.log("Array of Integers : " + arr);
+    console.log("Maximum product is " + max_product);
+}
+
+
